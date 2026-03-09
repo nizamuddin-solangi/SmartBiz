d28734751eff0a9fea6b8e50a98097bb5b2a1387
nizamuddin-solangi <nizamuddinsolangi614@gmail.com>
2026-03-09 08:59:18 +0500

    intern

--- backend/dist/routes/review.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const review_controller_1 = require("../controllers/review.controller");
-const auth_1 = require("../middlewares/auth");
-const router = (0, express_1.Router)();
-router.post('/', auth_1.protect, review_controller_1.createReview);
-router.get('/business/:businessId', review_controller_1.getBusinessReviews);
-exports.default = router;
